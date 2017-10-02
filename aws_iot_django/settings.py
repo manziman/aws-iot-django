09076da1d31746c3eaa22ef60d9cc333ddd8b4c8
manziman <christophermmanzi@gmail.com>
2017-10-01 22:28:49 -0500

    Added more elastic beanstalk compatibility

--- aws_iot_django/settings.py
+++ aws_iot_django/settings.py
@@ -139,16 +139,24 @@ WSGI_APPLICATION = 'aws_iot_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'aws_iot_django',
-        'USER': 'aws_iot_django_admin',
-        'PASSWORD': os.environ['DB_PASS'],
-        'HOST': 'localhost',
-        'PORT': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
