60d33c19425905563b3082eb6814adb922d88aa2
manziman <christophermmanzi@gmail.com>
2017-10-03 20:42:47 -0500

    Added random secret key generation at deploy time.

--- aws_iot_django/settings.py
+++ aws_iot_django/settings.py
@@ -81,13 +81,12 @@ BOOTSTRAP3 = {
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'testkey'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['iot-django-dev.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*.*.elasticbeanstalk.com']
 
 
 # Application definition
