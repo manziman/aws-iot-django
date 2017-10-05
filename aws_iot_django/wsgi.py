c074101e08587d2632e9201a9151c19d3d3efdbc
manziman <christophermmanzi@gmail.com>
2017-10-04 23:54:12 -0500

    added random secret key generation.

--- aws_iot_django/wsgi.py
+++ aws_iot_django/wsgi.py
@@ -7,14 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os, random, string
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aws_iot_django.settings")
-try:
-	sec = os.environ['SECRET_KEY']
-except:
-	os.environ.setdefault("SECRET_KEY", ''.join(random.choice(string.letters + string.digits) for i in range(16)))
 
 application = get_wsgi_application()
