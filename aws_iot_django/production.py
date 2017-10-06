75910b249ebd1c03ae7904e204bc8c96eb2333bd
manziman <christophermmanzi@gmail.com>
2017-10-05 23:49:47 -0500

    testing secret key generation function

--- aws_iot_django/production.py
+++
@@ -1,9 +0,0 @@
-import os, random, string
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aws_iot_django.settings")
-
-# os.environ.setdefault("SECRET_KEY", ''.join(random.choice(string.ascii_letters + string.digits) for i in range(16)))
-
-from django.core.wsgi import get_wsgi_application
-
-application = get_wsgi_application()
